0d0ae363cb1594a85d2dff17b9a209e9f97421f5
SermusAnton <antonsermus@mail.ru>
2018-05-07 14:34:42 +0300

    Release TF_IDF

--- SimpleLINQ/TF_IDF.cs
+++ SimpleLINQ/TF_IDF.cs
@@ -9,22 +9,34 @@ namespace SimpleLINQ
     class TF_IDF
     {
         public string[] Lines { get; set; }
-        public List<Dictionary<string, double>> GetTfidf() {
+        public Dictionary<string, double> GetTfidf() {
             var regex = new Regex("\\w+", RegexOptions.Compiled);
             var data = Lines
                 .Select(line => regex.Matches(line)
                     .OfType<Match>()  // Для приведения к типу с поддержкой IEnumerable<T>
                     .Select(m => m.Value)
-                    //.Where(s => !string.IsNullOrWhiteSpace(s))
                     .Select(s => s.ToLower())
                     .ToArray())
                 .ToArray();
-            // SelectMany - общая последовательность из нескольких
-            var words = data.SelectMany(d => d).Distinct().OrderBy(s => s).ToArray();
-
-
-
-            return null;// List<Dictionary<string,double>>  ;
+            // SelectMany - общая последовательность из нескольких вложенных массивов
+            int countWords = data.SelectMany(v => v).Count();
+            var wordsTF =
+                data.SelectMany(d => d)
+                .OrderBy(s => s)
+                .GroupBy(t=>t)
+                .Select(group=> new {WKey= group.Key, WTF =((double)group.Count() / countWords) });
+            // Корпусом (или документом) будет считаться абзац, тоесть элемент массива Lines
+            int countDoc = data.Count();
+            var wordsIDF =
+                data.Select(parag => parag.Distinct())
+                .SelectMany(d1 => d1)
+                .OrderBy(s2 => s2)
+                .GroupBy(r => r)
+                .Select(group1 => new { WKey = group1.Key, WIDF = Math.Log10(countDoc / (double)group1.Count()) });
+            Dictionary<string, double> TF_IDF =
+                wordsTF.Join(wordsIDF, wTF => wTF.WKey, wIDF => wIDF.WKey, (wTF, wIDF) => (wTF.WKey, wTF.WTF * wIDF.WIDF))
+                .ToDictionary(gh=>gh.Item1, gh=>gh.Item2);
+            return TF_IDF;
         }
     }
 }
