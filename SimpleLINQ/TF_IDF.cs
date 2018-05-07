427c60fc1ee165e36a43206b6544a16cabcc503d
SermusAnton <antonsermus@mail.ru>
2018-05-07 09:55:05 +0300

    Start Code

---
+++ SimpleLINQ/TF_IDF.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SimpleLINQ
+{
+    class TF_IDF
+    {
+        public string[] Lines { get; set; }
+        public List<Dictionary<string, double>> GetTfidf() {
+            var regex = new Regex("\\w+", RegexOptions.Compiled);
+            var data = Lines
+                .Select(line => regex.Matches(line)
+                    .OfType<Match>()  // Для приведения к типу с поддержкой IEnumerable<T>
+                    .Select(m => m.Value)
+                    //.Where(s => !string.IsNullOrWhiteSpace(s))
+                    .Select(s => s.ToLower())
+                    .ToArray())
+                .ToArray();
+            // SelectMany - общая последовательность из нескольких
+            var words = data.SelectMany(d => d).Distinct().OrderBy(s => s).ToArray();
+
+
+
+            return null;// List<Dictionary<string,double>>  ;
+        }
+    }
+}
